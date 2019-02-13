50f29be3867d055d1b8eb92a5be60b75ba911088
Anthony Clifton <aclifton@pillartechnology.com>
2019-02-13 13:34:20 -0600

    Base backend project

---
+++ backend/src/main/java/com/acliftonpillar/animalspottings/AnimalSpottingsApplication.java
@@ -0,0 +1,14 @@
+package com.acliftonpillar.animalspottings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalSpottingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalSpottingsApplication.class, args);
+	}
+
+}
+
